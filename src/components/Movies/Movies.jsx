0d31dbf948198bddb8a3c3e404d45eef61c5c68c
Julien Sarda <104718280+JUJUdev1@users.noreply.github.com>
2022-12-16 15:29:04 +0100

    rendered movies

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -1,15 +1,44 @@
 import React, { useEffect, useState } from 'react';
-import { Box, CircularProgress, useMediaQuery,Typography } from '@mui/material';
+import {
+  Box,
+  CircularProgress,
+  useMediaQuery,
+  Typography,
+} from '@mui/material';
 import { useSelector } from 'react-redux';
 
 import { useGetMoviesQuery } from '../../services/TMDB';
+import { MovieList } from '..';
 
 const Movies = () => {
-  const { data } = useGetMoviesQuery();
-  console.log(data)
-  return (
-    <div>Movies</div>
-  )
-}
+  const { data, error, isFetching } = useGetMoviesQuery();
 
-export default Movies
+  if (isFetching)
+    return (
+      <Box display='flex' justifyContent='center'>
+        <CircularProgress size='4rem' />
+      </Box>
+    );
+
+  if (!data.results.length) {
+    return (
+      <Box display='flex' alignItems='center' mt='20px'>
+        <Typography variant='h4'>
+          No movies that match that name.
+          <br />
+          Please search for something else.
+        </Typography>
+      </Box>
+    );
+  }
+
+  if (error) return 'An error has occurred';
+
+  return (
+    <div>
+      <MovieList movies={data} />
+    </div>
+  );
+};
+
+export default Movies;
