8ead026abff3b7f47e2e9714df247cee5e562741
Julien Sarda <104718280+JUJUdev1@users.noreply.github.com>
2022-12-16 14:15:22 +0100

    sidebar fonctionnality setup

--- src/components/NavBar/styles.js
+++ src/components/NavBar/styles.js
@@ -1,5 +1,7 @@
 import { makeStyles } from '@mui/styles';
 
+const drawerWidth = 240;
+
 export default makeStyles((theme) => ({
     toolbar: {
         height: '80px',
@@ -16,5 +18,20 @@ export default makeStyles((theme) => ({
         [theme.breakpoints.up('sm')]: {
             display: 'none',
     },
+    },
+    drawer: {
+        [theme.breakpoints.up('sm')]: {
+            width: drawerWidth,
+            flexShrink: 0,
+        },
+    },
+    drawerPaper: {
+        width: drawerWidth,
+    },
+    linkButton: {
+        '&:hover': {
+            color: 'white !important',
+            textDecoration: 'none',
+        },
     },
 }));
