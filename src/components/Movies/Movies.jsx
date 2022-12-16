f81055dc86cc1724287a129e4e9f801814e299d3
Julien Sarda <104718280+JUJUdev1@users.noreply.github.com>
2022-12-16 15:04:23 +0100

    connected data with redux

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -1,6 +1,12 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { Box, CircularProgress, useMediaQuery,Typography } from '@mui/material';
+import { useSelector } from 'react-redux';
+
+import { useGetMoviesQuery } from '../../services/TMDB';
 
 const Movies = () => {
+  const { data } = useGetMoviesQuery();
+  console.log(data)
   return (
     <div>Movies</div>
   )
