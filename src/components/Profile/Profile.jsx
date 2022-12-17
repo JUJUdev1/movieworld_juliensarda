28062f1adebd03d17be3e4952e5c5831246fbc2d
Julien Sarda <104718280+JUJUdev1@users.noreply.github.com>
2022-12-17 15:14:00 +0100

    added log out and profile fectch

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,12 +1,42 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import { Box, Typography, Button } from '@mui/material';
+import { useSelector } from 'react-redux';
+import { ExitToApp } from '@mui/icons-material';
+
+import { userSelector } from '../../features/auth';
 
 // Get access to profile name or id from redux state
 // display in the profile component
 
 const Profile = () => {
-  return (
-    <div>Profile</div>
-  )
-}
+  const { user } = useSelector(userSelector);
 
-export default Profile
+  const favoriteMovies = [];
+
+  const logout = () => {
+    localStorage.clear();
+    window.location.href = '/';
+  };
+
+  return (
+    <Box>
+      <Box display='flex' justifyContent='space-between'>
+        <Typography variant='h4' gutterBottom>
+          My profile
+        </Typography>
+        <Button color='inherit' onClick={logout}>
+          Logout &nbsp; <ExitToApp />
+        </Button>
+      </Box>
+      {!favoriteMovies.length ? (
+        <Typography variant='h5'>
+          Add favorites or watchlist some movies to see them here!
+        </Typography>
+      ) : (
+        <Box>FAVORITE MOVIES</Box>
+      )}
+    </Box>
+  );
+};
+
+export default Profile;
